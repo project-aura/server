d34282dde7d531da8e4f34be47dcd23df7c2bdd0
erv <ervinmitra@gmail.com>
2019-03-05 10:00:01 -0800

    added files

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
+const businessRouter = require('./src/dataSnatcher');
 
 const app = express();
+const pathToYelpAPI = `https://api.yelp.com/v3/businesses/`
+
+/* Added on 3/5/2019
+ * 1. body parser middleware
+ * 2. form data
+ * 3. CORS middleware
+ */
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+
+// static middleware
+app.use(express.static(path.join(__dirname, "public")));
+/* MIGHT need to put some STATIC MIDDLEWARE IN HERE (Optional)
+ * Route and Set endpoints. 
+ * BE MINDFUL OF the 1st parameter! Seriously!
+ */
+app.use(pathToYelpAPI, businessRouter);
 
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
