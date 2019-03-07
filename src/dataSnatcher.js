adadc2ed3411d36f15ae64842d964f49787b9106
emily-yang <cigoua@gmail.com>
2019-03-07 13:56:15 -0800

    add get request for aura list

--- src/dataSnatcher.js
+++ src/dataSnatcher.js
@@ -1,24 +1,31 @@
-/* dataSnatcher.js - snatches data from the business API 
+/* dataSnatcher.js - snatches data from the business API
  */
 const express = require('express');
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const businessLA = require('../sample-data/los-angeles-data/businessLA');
+const auraList = require('../sample-data/aura/auras.json');
 
 /* Create the DB, if it doesn't exist, then seed it
- * with data. 
+ * with data.
  * Then sync the adapter and router, and then create router
  */
 const adapter = new FileSync('businessLA.json', {
-    defaultValue: {businessData: businessLA},
+  defaultValue: { businessData: businessLA },
 });
+
 const db = lowdb(adapter);
 const router = express.Router();
 
 // GET request
 router.get('/', (req, res) => {
-    let businessData = db.get('businessData').value();
-    res.json(businessData);
-})
+  const businessData = db.get('businessData').value();
+  res.json(businessData);
+});
+
+// GET request for aura list
+router.get('/auras', (req, res) => {
+  res.json(auraList);
+});
 
 module.exports = router;
