e6771bcfb580f6a0c5ef81ab24ad560fd5a53b6c
erv <ervinmitra@gmail.com>
2019-03-19 12:30:32 -0700

    modularized dbAdd, added tests, tests suceeded

--- src/dataMaster.js
+++ src/dataMaster.js
@@ -3,18 +3,28 @@
  */
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-//const adapter = new FileSync('businessLA.json');
-const adapter = new FileSync('dummyDB.json');
-const db = lowdb(adapter);
 
 const dataMaster = {
-    // add to database
-    dbAdd: (dbJsonName, fileSyncer, objectEntry) => {
-        //db.get('businessData')
-        db.get('businessDataFake')
+    // ===========================add to database===============================
+    /* dbAdd parameters
+     * @nameDBJSON -> this is the name of the JSON file DB passed on to FileSync
+     * @nameJSONParent -> this is the name of array that holds the JSON objects
+     *                      in the database. Yes, nameJSONParent is inside
+     *                      of nameDBJSON.
+     * @objectEntry -> the object to be added into the database.
+     * NOTE -> FileSync(nameDBJSON) knows exactly where to find your file.
+     * There is NO NEED to specify pathname. Crazy I know. 
+     */
+    dbAdd: (nameDBJSON, nameJSONParent ,objectEntry) => {
+        // FileSync and adapter integration in this section
+        // instead of close to the head part
+        const adapter = new FileSync(nameDBJSON);
+        const db = lowdb(adapter);
+        db.get(nameJSONParent)
             .push(objectEntry)
             .write();
     }
+    // ==========================================================================
 }
 
 module.exports = {
