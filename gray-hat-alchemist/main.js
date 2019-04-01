669bc088a2d82f98e21506157cafab8f94d02bec
erv <ervinmitra@gmail.com>
2019-04-01 09:31:30 -0700

    mongo updates

--- gray-hat-alchemist/main.js
+++ gray-hat-alchemist/main.js
@@ -1,5 +1,4 @@
 const { alchemicSearch } = require('./src/alchemicSearch');
-const addr = 'https://www.yelp.com/biz/the-serving-spoon-inglewood';
 
 invokeMysticalPowers = (urlParam) => {
     return alchemicSearch(urlParam);
--- tests/testDataMaster.js
+++ tests/testDataMaster.js
@@ -1,29 +1,107 @@
 /**
  *  TEST THE DATA MASTER
  */
+const express = require('express');
+const DataMaster  = require('./../src/DataMaster');
 
-const { DataMaster } = require('./../src/DataMaster');
+// setup express router
+const router = express.Router();
 
-insertedFakeData = {
-  name: 'Fake2',
-  address: 'Fake St2.',
-  city: 'Fake City2',
-  state: 'Fake State2',
-  postal_code: 'Fake Zip2',
-  attributes: {
-    Aura: 'fake2',
+const data = {
+  "id": "EL12403cpcEf8hLYXSpptg",
+  "name": "Bay Cities Italian Deli",
+  "alias": "bay-cities-italian-deli-santa-monica",
+  "address": "1517 Lincoln Blvd",
+  "city": "Santa Monica",
+  "state": "CA",
+  "postalCode": "90401",
+  "latitude": 34.017994,
+  "longitude": -118.489223,
+  "url": "https://www.yelp.com/biz/bay-cities-italian-deli-santa-monica?adjust_creative=y-XxUFQR_0WfAHaWdPDt2w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=y-XxUFQR_0WfAHaWdPDt2w",
+  "stars": 4,
+  "reviewCount": 4397,
+  "novelty": 0,
+  "businessImage": {
+    "src": "https://images2.laweekly.com/imager/bay-cities/u/original/4461347/5606707.0.jpg",
+    "owner": "LA Weekly"
   },
-  img: 'fakeimageurl2',
-  categories: 'faking2',
-  fake_category: 'a fake category, succeeds if added2',
-};
+  "attributes": {
+    "aura": "trendy",
+    "wifi": "",
+    "alcohol": "",
+    "bestNights": {
+      "monday": false,
+      "tuesday": false,
+      "wednesday": false,
+      "thursday": false,
+      "friday": false,
+      "saturday": false,
+      "sunday": false
+    },
+    "goodForKids": false,
+    "goodForDancing": false,
+    "goodForMeal": {
+      "breakfast": false,
+      "brunch": false,
+      "lunch": false,
+      "dinner": false,
+      "lateNight": false,
+      "dessert": false
+    },
+    "priceRange": "$",
+    "parking": {
+      "garage": false,
+      "street": false,
+      "lot": false,
+      "valet": false,
+      "validated": false
+    },
+    "noiseLevel": "",
+    "attire": "",
+    "hasTV": false,
+    "music": {
+      "jukebox": false,
+      "video": false,
+      "karoake": false,
+      "dj": false,
+      "live": false,
+      "backgroundMusic": false,
+      "noMusic": false
+    },
+    "smoking": ""
+  },
+  "categories": [
+    {
+      "alias": "delis",
+      "title": "Delis"
+    },
+    {
+      "alias": "sandwiches",
+      "title": "Sandwiches"
+    },
+    {
+      "alias": "italian",
+      "title": "Italian"
+    }
+  ],
+  "displayAddress": [
+    "1517 Lincoln Blvd",
+    "Santa Monica, CA 90401"
+  ]
+}
 
-// test addEntry()
-const database = new DataMaster('businessLAFake.json');
-database.addEntry(insertedFakeData);
+// GET request
+router.get('/businesses', (req, res) => {
+  dataMaster.find(req,res);
+})
 
-// test seed()
-const businessLAFake = require('./businessLAFake');
-// import the array
-const db2 = new DataMaster('moreJSONs.json');
-db2.seed(businessLAFake);
+const dataMaster = new DataMaster();
+// test addToEntry
+//dataMaster.seed(businessLA);
+
+// test findByAlias
+// dataMaster.findByAlias(data.alias);
+
+module.exports = router;
+
+ 
