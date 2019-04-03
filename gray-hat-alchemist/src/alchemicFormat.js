2cc712f063d130499a0744b68e690bba4f78069a
erv <ervinmitra@gmail.com>
2019-04-03 10:17:35 -0700

    injected steroids into scraper

--- gray-hat-alchemist/src/alchemicFormat.js
+++ gray-hat-alchemist/src/alchemicFormat.js
@@ -3,51 +3,20 @@
  * Output -> a string that represents aura
  */
 
- alchemicFormat = (text) => {
-    /* How this works:
-    * Search for the occurences of the auras:
-    * Trendy, Cheerful, Inspired, Romantic,
-    * Peaceful, Classy, Hipster, 
-    * Spiritual, Silly, Touristy, 
-    * Exotic, Upscale
-    * The following variables are constant
-    * representations of the auras
-    */
-    const trendy = 'trendy';
-    const cheerful = 'cheerful';
-    const inspired = 'inspired';
-    const romantic = 'romantic';
-    const peaceful = 'peaceful';
-    const classy = 'classy';
-    const hipster = 'hipster';
-    const spiritual = 'spiritual';
-    const silly = 'silly';
-    const touristy = 'touristy';
-    const exotic = 'exotic';
-    const upscale = 'upscale';
-    const casual = 'casual';
-    const lively = 'lively';
-
-    let aura;
-    if(text.includes(cheerful)){ aura = cheerful }
-    else if(text.includes(inspired)){ aura = inspired }
-    else if(text.includes(romantic)){ aura = romantic }
-    else if(text.includes(peaceful)){ aura = peaceful }
-    else if(text.includes(classy)){ aura = classy }
-    else if(text.includes(hipster)){ aura = hipster }
-    else if(text.includes(spiritual)){ aura = spiritual }
-    else if(text.includes(silly)){ aura = silly }
-    else if(text.includes(touristy)){ aura = touristy }
-    else if(text.includes(exotic)){ aura = exotic }
-    else if(text.includes(upscale)){ aura = upscale }
-    else if(text.includes(casual)){ aura = casual }
-    else if(text.includes(lively)){ aura = lively }
-    else{ aura = trendy }
-
-    return aura;
+ /**
+  * @param {text} -> the argument passed into the function. 
+  * 1. convert text to lower case
+  * 2. trim the garbage tags in front
+  * 3. trim the garbage tags in back
+  * 4. return
+  */
+ alchemicFormat = text => {
+    let returnedAura = text.toLowerCase();
+    returnedAura = returnedAura.substr(9, 70).trim();
+    const indexOfAngleBracket = returnedAura.indexOf('<');
+    returnedAura = returnedAura.substr(0, indexOfAngleBracket).trim();
+    return returnedAura;
 }
 
-module.exports = {
-    alchemicFormat,
-};
+module.exports = alchemicFormat;
  
---
+++ gray-hat-alchemist/tests/testSpider.js
@@ -0,0 +1,48 @@
+const invokeMysticalPowers = require('../main');
+
+const addr = 'https://www.yelp.com/biz/howlin-rays-los-angeles-3';
+const addr2 = 'https://www.yelp.com/biz/wurstk%C3%BCche-los-angeles-2';
+const addr3 = 'https://www.yelp.com/biz/bestia-los-angeles';
+const addr4 = 'https://www.yelp.com/biz/the-morrison-los-angeles';
+const addr5 = 'https://www.yelp.com/biz/langers-los-angeles-2';
+const addr6 = 'https://www.yelp.com/biz/r%C3%A9publique-los-angeles-2?osq=Restaurants';
+const addr7 = 'https://www.yelp.com/biz/running-goose-los-angeles?osq=Restaurants';
+const addr8 = 'https://www.yelp.com/biz/wako-donkasu-los-angeles';
+const addr9 = 'https://www.yelp.com/biz/din-tai-fung-los-angeles?osq=Restaurants';
+const addr10 = 'https://www.yelp.com/biz/eggslut-los-angeles-7?osq=Restaurants';
+const addr11 = 'https://www.yelp.com/biz/broken-mouth-lees-homestyle-los-angeles-5?osq=Restaurants';
+const addr12 = 'https://www.yelp.com/biz/the-morrison-los-angeles?osq=Restaurants';
+const addr13 = 'https://www.yelp.com/biz/jokbal-1030-los-angeles-2?osq=Restaurants';
+const addr14 = 'https://www.yelp.com/biz/met-her-at-a-bar-los-angeles-2?osq=Restaurants';
+const addr15 = 'https://www.yelp.com/biz/bao-dim-sum-house-los-angeles?osq=Restaurants';
+
+invokeMysticalPowers(addr)
+     .then(x => console.log(`1: ${x}`));
+invokeMysticalPowers(addr2)
+     .then(x => console.log(`2: ${x}`));
+invokeMysticalPowers(addr3)
+     .then(x => console.log(`3: ${x}`));
+invokeMysticalPowers(addr4)
+     .then(x => console.log(`4: ${x}`));
+invokeMysticalPowers(addr5)
+     .then(x => console.log(`5: ${x}`));
+invokeMysticalPowers(addr6)
+     .then(x => console.log(`6: ${x}`));
+invokeMysticalPowers(addr7)
+     .then(x => console.log(`7: ${x}`));
+invokeMysticalPowers(addr8)
+     .then(x => console.log(`8: ${x}`));
+invokeMysticalPowers(addr9)
+     .then(x => console.log(`9: ${x}`));
+invokeMysticalPowers(addr10)
+     .then(x => console.log(`10: ${x}`));
+invokeMysticalPowers(addr11)
+     .then(x => console.log(`11: ${x}`));
+invokeMysticalPowers(addr12)
+     .then(x => console.log(`12: ${x}`));
+invokeMysticalPowers(addr13)
+     .then(x => console.log(`13: ${x}`));
+invokeMysticalPowers(addr14)
+     .then(x => console.log(`14: ${x}`));
+invokeMysticalPowers(addr15)
+     .then(x => console.log(`15: ${x}`));
