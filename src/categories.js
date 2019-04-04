b1c372f6523b421ddc3cc6b8b43c48fa7f18060e
Jonathan Azali <azali.jonathan@gmail.com>
2019-04-04 15:33:35 -0700

    drinking categories updated

--- src/categories.js
+++ src/categories.js
@@ -1,6 +1,8 @@
 const categories = {
   eating: 'italian, american, newamerican',
-  drinking: 'gastropub, coffee'
+  studying: '',
+  drinking:
+    'beer, wine, spirits, breweries, brewpubs, bubbletea, cideries, coffee, coffeeroasteries, juicebars, kombucha, meaderies, milkshakebars, gluhwein, tea, wineries, bars, absinthebars, airportlounges, beachbars, champagene_bars, cigarbars, cocktailbars, divebars, drivethrubars, gaybars, hookah_bars, hotel_bar, irish_pubs, lounges, pubs, pulquerias, sakebars, speakeasies, sportsbars, tabac, tikibars, vermouthbars, wine_bars, beergardens'
 };
 
 module.exports = categories;
