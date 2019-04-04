b37988f9522dd3aa153758991b4d6d414deae498
Jonathan Azali <azali.jonathan@gmail.com>
2019-04-04 15:46:39 -0700

    more updates

--- src/categories.js
+++ src/categories.js
@@ -1,8 +1,12 @@
 const categories = {
   eating: 'italian, american, newamerican',
-  studying: '',
+  studying: 'bubbletea, coffee, coffeeroasteries, juicebars, libraries',
+  dating:
+    'clubcrawl, countrydancehalls, danceclubs, dancerestaurants,jazzandblues, karaoke, musicvenues, pianobars, artmuseums, musicvenues, paintandsip',
+  relaxing:
+    'meditationcenters, yoga, jazzandblues, mahjong, observatories, opera, pachinko, paintandsip, theater, planetarium, sportsbetting',
   drinking:
-    'beer, wine, spirits, breweries, brewpubs, bubbletea, cideries, coffee, coffeeroasteries, juicebars, kombucha, meaderies, milkshakebars, gluhwein, tea, wineries, bars, absinthebars, airportlounges, beachbars, champagene_bars, cigarbars, cocktailbars, divebars, drivethrubars, gaybars, hookah_bars, hotel_bar, irish_pubs, lounges, pubs, pulquerias, sakebars, speakeasies, sportsbars, tabac, tikibars, vermouthbars, wine_bars, beergardens'
+    'beer, wine, spirits, breweries, brewpubs, bubbletea, cideries, coffee, coffeeroasteries, juicebars, kombucha, meaderies, milkshakebars, gluhwein, tea, wineries, bars, absinthebars, airportlounges, beachbars, champagene_bars, cigarbars, cocktailbars, divebars, drivethrubars, gaybars, hookah_bars, hotel_bar, irish_pubs, lounges, pubs, pulquerias, sakebars, speakeasies, sportsbars, tabac, tikibars, vermouthbars, wine_bars, beergardens, paintandsip'
 };
 
 module.exports = categories;
