3dd4372103d88def1e3cd3fb422655116fc398eb
Scott Roberts <s.e.roberts0@gmail.com>
2019-04-23 10:24:51 -0700

    Controller documentation

---
+++ controllers/business.controller.js
@@ -0,0 +1,79 @@
+/**
+ * Creates a single business
+ * @param {Object} business Aura Business
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const createOne = (business, options) => {};
+
+/**
+ * Creates many businesses with a batch request
+ * @param {Object} businesses Aura Businesses
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const createMany = (businesses, options) => {};
+
+/**
+ * Reads a single business
+ * @param {Object} business Aura Business
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const readOne = (business, options) => {};
+
+/**
+ * Reads many businesses with a batch request
+ * @param {Object} businesses Aura Businesses
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const readMany = (businesses, options) => {};
+
+/**
+ * Updates a single business
+ * @param {Object} business Aura Business
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const updateOne = (business, options) => {};
+
+/**
+ * Updates many businesses with a batch request
+ * @param {Object} businesses Aura Businesses
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const updateMany = (businesses, options) => {};
+
+/**
+ * Deletes a single business
+ * @param {Object} business Aura Business
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const deleteOne = (business, options) => {};
+
+/**
+ * Deletes many businesses with a batch request
+ * @param {Object} businesses Aura Businesses
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const deleteMany = (businesses, options) => {};
+
+/**
+ * Deletes many businesses then addes many businesses with a batch request
+ * @param {Object} businesses Aura Businesses
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const seed = (businesses, options) => {};
+
+/**
+ * Finds businesses by query
+ * @param {Object} query Search object parameters
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const find = (query, options) => {};
---
+++ controllers/feedback.controller.js
@@ -0,0 +1,79 @@
+/**
+ * Creates a single feedback
+ * @param {Object} feedback Aura feedback
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const createOne = (feedback, options) => {};
+
+/**
+ * Creates many feedback with a batch request
+ * @param {Object} feedback Aura feedbackes
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const createMany = (feedback, options) => {};
+
+/**
+ * Reads a single feedback
+ * @param {Object} feedback Aura feedback
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const readOne = (feedback, options) => {};
+
+/**
+ * Reads many feedbackes with a batch request
+ * @param {Object} feedback Aura feedbackes
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const readMany = (feedback, options) => {};
+
+/**
+ * Updates a single feedback
+ * @param {Object} feedback Aura feedback
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const updateOne = (feedback, options) => {};
+
+/**
+ * Updates many feedback with a batch request
+ * @param {Object} feedback Aura feedbackes
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const updateMany = (feedback, options) => {};
+
+/**
+ * Deletes a single feedback
+ * @param {Object} feedback Aura feedback
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const deleteOne = (feedback, options) => {};
+
+/**
+ * Deletes many feedback with a batch request
+ * @param {Object} feedback Aura feedbackes
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const deleteMany = (feedback, options) => {};
+
+/**
+ * Deletes many feedback then addes many feedbackes with a batch request
+ * @param {Object} feedback Aura feedbackes
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const seed = (feedback, options) => {};
+
+/**
+ * Finds feedback by query
+ * @param {Object} query Search object parameters
+ * @param {Object} options Additional parameters
+ * @returns Response
+ */
+const find = (query, options) => {};
