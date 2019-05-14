1eee3acdc86dc2af32079d647cb15078bb9fcb58
erv <ervinmitra@gmail.com>
2019-05-14 10:03:38 -0700

    city search initialize

---
+++ tests/testCitySearch.js
@@ -0,0 +1,14 @@
+/**
+ * This file 
+ * 1. takes all of the businesses
+ * 2. reads the postal code of each business
+ * 3. figures out the city of the business from the given postal code
+ * 4. saves it in the field called 'citySearch'
+ * 5. saves it back to the DB
+ * That was a mouthful
+ */
+
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '/../.env') });
+const businessController = require('../controllers/business.controller');
+const DataMaster = require('../controllers/DataMaster');
