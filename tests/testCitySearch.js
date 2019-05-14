64b55f6268a160362bd5ef3d93d3de181c60e182
erv <ervinmitra@gmail.com>
2019-05-14 10:04:45 -0700

    initialize connection

--- tests/testCitySearch.js
+++ tests/testCitySearch.js
@@ -12,3 +12,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const businessController = require('../controllers/business.controller');
 const DataMaster = require('../controllers/DataMaster');
+
+// connect to DB
+const dataMaster = new DataMaster(process.env.ENVIRONMENT);
+dataMaster.connectForMutations(process.env.ENVIRONMENT);
