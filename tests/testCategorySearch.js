a71b28e682b733fb8bc9583e19db5fa7a654c069
erv <ervinmitra@gmail.com>
2019-05-15 09:58:32 -0700

    initialize category search

---
+++ tests/testCategorySearch.js
@@ -0,0 +1,10 @@
+/**
+ * This file
+ * 1. takes all of the businesses
+ * 2. reads the categories array of each business
+ * 3. figures out the category of the business from the given subcategories
+ * 4. saves it in the field called 'categorySearch'
+ * 5. saves it back to the DB. -> Call updateOne() save each time its
+ * updated.
+ * That was a mouthful
+ */
