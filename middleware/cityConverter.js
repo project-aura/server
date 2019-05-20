4b2060d91d3df7b2e52a3a5b774d877ccea56998
erv <ervinmitra@gmail.com>
2019-05-20 11:04:28 -0700

    changed instructions

--- middleware/cityConverter.js
+++ middleware/cityConverter.js
@@ -1,13 +1,12 @@
 /**
- * City Converter - converts the cities from the businesses
- * 1. takes all of the businesses
- * 2. reads the postal code of each business
- * 3. figures out the city of the business from the given postal code
- * 4. saves it in the field called 'citySearch'
- * 5. returns the businesses with a field called 'citySearch'
+ * Converters - converts attributes from the businesses
+ * appends fields into the businesses depending on the function that is called
+ * citySearch and categorySearch are fields that it will be appended to
+ * since the business models have been updated to have these fields.
  */
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const businessController = require('../controllers/business.controller');
 const DataMaster = require('../controllers/DataMaster');
 const zipCodes = require('../data/zipCodes');
+
