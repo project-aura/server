fada4bd528645efb44c5e68a02ea4caace3df992
erv <ervinmitra@gmail.com>
2019-05-20 10:49:26 -0700

    initialize city conversion

---
+++ middleware/cityConverter.js
@@ -0,0 +1,8 @@
+/**
+ * City Converter - converts the cities from the businesses
+ * 1. takes all of the businesses
+ * 2. reads the postal code of each business
+ * 3. figures out the city of the business from the given postal code
+ * 4. saves it in the field called 'citySearch'
+ * 5. saves it back to the DB. -> Call updateOne() save each time its
+ */
