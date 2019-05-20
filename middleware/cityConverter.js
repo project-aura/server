a921a86cf84c14334fb914b07e9fd1f519c9e007
erv <ervinmitra@gmail.com>
2019-05-20 10:56:00 -0700

    initialize data master

--- middleware/cityConverter.js
+++ middleware/cityConverter.js
@@ -9,3 +9,4 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const businessController = require('../controllers/business.controller');
+const DataMaster = require('../controllers/DataMaster');
