e5f92bf1979f0df35d9cb43cfa1d5796844ce532
erv <ervinmitra@gmail.com>
2019-05-20 11:25:29 -0700

    sync code

--- middleware/converter.js
+++ middleware/converter.js
@@ -11,7 +11,65 @@ const zipCodes = require('../data/zipCodes');
 const categories = require('../data/categories');
 
 const cityConvert = async (businesses) => {
-    const returnedBusinesses;
+    const returnedBusinesses = await businesses.forEach(business => {
+        let formattedCity;
+        if(business.city !== 'Los Angeles') {
+            formattedCity = business.city;
+        }
+        else {
+            // city as Los Angeles needs to be curated 
+            // further based of postal code
+            // target a business' postal Code
+            let zip;
+            for(let key in zipCodes) {
+                if(zipCodes[key].includes(business.postalCode)) {  
+                    zip = key;
+                    break;
+                }
+            }
+            // format returned key from map
+            if(zip === 'santaMonica') {
+                formattedCity = 'Santa Monica';
+            } else if(zip === 'DTLA') {
+                formattedCity = 'Downtown Los Angeles';
+            } else if(zip === 'culverCity') {
+                formattedCity = 'Culver City';
+            } else if(zip === 'beverlyHills') {
+                formattedCity = 'Beverly Hills';
+            } else if(zip === 'hollywood') {
+                formattedCity = 'Hollywood';
+            } else if(zip === 'vanNuys') {
+                formattedCity = 'Van Nuys';
+            } else if(zip === 'pasadena') {
+                formattedCity = 'Pasadena';
+            } else if(zip === 'newportBeach') {
+                formattedCity = 'Newport Beach';
+            } else if(zip === 'laBrea') {
+                formattedCity = 'La Brea';
+            } else if(zip === 'anaheim') {
+                formattedCity = 'Anaheim';
+            } else if(zip === 'rowlandHeights') {
+                formattedCity = 'Rowland Heights';
+            } else if(zip === 'brea') {
+                formattedCity = 'Brea';
+            } else if(zip === 'laguna') {
+                formattedCity = 'Laguna';
+            } else if(zip === 'huntingtonBeach') {
+                formattedCity = 'Huntington Beach';
+            } else if(zip === 'costaMesa') {
+                formattedCity = 'Costa Mesa';
+            } else if(zip === 'gardenGrove') {
+                formattedCity = 'Garden Grove';
+            } else {
+                formattedCity = 'Greater Los Angeles';
+            }
+        }
+        
+        // do updates in here then lol
+        businessController.updateOne(business._id, {
+            citySearch: formattedCity,
+        });
+    });
 
     return returnedBusinesses;
 }
