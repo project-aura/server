a148384c4daad0439827747de6dfc1f12a36a265
erv <ervinmitra@gmail.com>
2019-06-04 12:27:17 -0700

    feedback model under construction

--- models/feedback.model.js
+++ models/feedback.model.js
@@ -1,17 +1,45 @@
+/**
+ * A feedback is between a user and a business. It must be associated with 
+ * both entities to exist. 
+ * 
+ */
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema(
   {
     user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
+      userId: String,
+      objectReference: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      }
     },
     business: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'business',
+      businessId: String,
+      objectReference: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'business',
+      },
     },
-    auras: mongoose.Schema.Types.Mixed,
-    checkins: Number,
+    // aura, activity and attire are arrays. Only present 
+    // within the context of the user voting for these
+    // in the specific business.
+    aura: [
+      {
+        type: String,
+      },
+    ],
+    activity: [
+      {
+        type: String,
+      },
+    ],
+    attire: [
+      {
+        type: String,
+      },
+    ],
+    checkins: Number, // number of times the user checked in the business
     comment: String,
   },
   {
