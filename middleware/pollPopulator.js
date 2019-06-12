8cfeb0e87ef240b5ee95d95db6251a97482390d2
erv <ervinmitra@gmail.com>
2019-06-12 13:43:37 -0700

    path and require path initialized

--- middleware/pollPopulator.js
+++ middleware/pollPopulator.js
@@ -2,3 +2,6 @@
  * Poll populator populates the poll based of aura 
  * from the webscraper and the categorySearch field.
  */
+
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '/../.env') });
