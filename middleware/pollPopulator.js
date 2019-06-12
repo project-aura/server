13f849c9ca1c056b8200863e2a4ebba26f202adc
erv <ervinmitra@gmail.com>
2019-06-12 13:46:05 -0700

    initialized businessController

--- middleware/pollPopulator.js
+++ middleware/pollPopulator.js
@@ -6,4 +6,5 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const DataMaster = require('../controllers/DataMaster');
+const businessController = require('../controllers/business.controller');
 
