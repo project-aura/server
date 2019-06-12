56082c6f2deaaca8c043b6cdabcfff46d4d4944c
erv <ervinmitra@gmail.com>
2019-06-12 14:47:40 -0700

    initialize poll aura voting

--- middleware/pollPopulator.js
+++ middleware/pollPopulator.js
@@ -12,12 +12,61 @@ const dataMaster = new DataMaster(process.env.ENVIRONMENT);
 dataMaster.connectForMutations(process.env.ENVIRONMENT);
 
 /**
- * 1. Obtain all the businesses in the database (readMany)
+ * for auras
+ * 1. READ all the businesses in the database (readMany)
  * 2. For each business, read the attributtes.aura field
  *      -> trim whitespace
  *      -> place each word into an array with the commas as delimiters
  *      -> for each entry in the array, find the appropriate key in the 
  *          auras field and increment that
- *      -> update the new auras field in the document
+ *      -> UPDATE the new auras field in the document (updateOne)
+ * ======================================================================
+ * for activities
+ * 1. READ all businesses in the database (readMany)
+ * 2. For each business, read the categorySearch field
+ *      -> trim whitespace
+ *      -> place each word into an array with the commas as delimiters
+ *      -> for each entry in the array, find the appropriate ket in the 
+ *          activities field and increment that
+ *      -> UPDATE the new activities field in the document (updateOne)
  */
+const populate = async () => {
+    const comma = ',';
+    const proc = await businessController.readMany({})
+        .then(businesses => {
+            for(let i = 0; i < businesses.length; ++i) {
+                let attributeAura = businesses[i].attributes.aura;
+                attributeAura = attributeAura.replace(/\s+/g, '');
+                // make the array, using commas as delimiters
+                let cursor = 0;
+                let toBeShovedInArray = '';
+                let auraArray = [];
+                while(cursor < attributeAura.length) {
+                    if(attributeAura[cursor] !== comma) {
+                        toBeShovedInArray += attributeAura[cursor];
+                    } else {
+                        // delimiter has been encountered
+                        auraArray.push(toBeShovedInArray);
+                        toBeShovedInArray = '';
+                    }
+                    cursor++;
+                }
+                // push one more for that last entry
+                auraArray.push(toBeShovedInArray);
+                
+                // on to the good stuff. SPREAD IT.... 
+                for(let k = 0; k < auraArray.length; ++k) {
+                    businesses[i].auras[auraArray[k]]++;
+                }
+                // DONE. update DB
+                businessController.updateOne(businesses[i]._id, {
+                    auras: businesses[i].auras,
+                });
+            }
+        });
+}
+
+populate();
+
+
 
